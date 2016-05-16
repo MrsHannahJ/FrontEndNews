3a087ff12f7107fcf86e601de370ef6ec7b382ff
Hannah Jones <mrs.hannahjones@gmail.com>
2016-05-16 14:02:47 -0700

    add modal functionality

--- app.js
+++ app.js
@@ -9,9 +9,28 @@ $(document).ready(function () {
     $(this).text('Liked!').closest('.news-item').addClass('is-liked');
   });
   
+  //-----------------------------------------------------
+//  ADD LINK
+//-----------------------------------------------------
+  
   $(".js-add-link").on("click", function(event){
     event.preventDefault();
     
     $(".js-form").toggleClass('is-visible');
   })
+  });
+
+//-----------------------------------------------------
+//  MODAL
+//-----------------------------------------------------
+  $(".js-show-modal").on("click", function(event){
+    event.preventDefault();
+    
+    $(".js-modal").addClass('is-visible');
+    $(".js-modal-overlay").addClass('is-visible');
+  });
+  $(".js-modal-overlay").on("click", function(event){
+    $(".js-modal").removeClass('is-visible');
+    $(".js-modal-overlay").removeClass('is-visible');
+
 });
